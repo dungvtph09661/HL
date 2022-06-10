81487dce07df4a1af561d52ea119d3ea970b1993
Vũ Tiến Dũng <61072709+dungvtph09661@users.noreply.github.com>
2022-06-11 00:53:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/ly.huong.169067';
+const eventName = 'Chúc Mừng Tốt Nghiệp Hương Ly';
+const titleCard = 'Lời Chúc All';
+const contentCard = '3 năm mẫu giáo “dấm đài”+ 12 năm phổ thông “hay ăn chóng lớn” + 4 năm đại học “lãng mạn sinh viên” + 2 năm thạc sĩ “vất vả thế” = thành công phía trước. Chân trời mới vẫn đang rộng mở chờ đón bạn. Luôn vui vẻ và hạnh phúc nhé!';
+
+const giftImage = 'NTHL2.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
